630ad788ce59f2a95aa0853ab03210822bf496b2
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-17 13:26:04 -0500

    all routes, folders, connections

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+// create connection to our databaase, pass in your MYSQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306
+});
+
+module.exports = sequelize;

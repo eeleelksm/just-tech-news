14847e096b3daf7c8a1e314ebb80a88701d85759
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-18 14:01:06 -0500

    redid missing files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
-router.use("/api", apiRoutes);
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).end();

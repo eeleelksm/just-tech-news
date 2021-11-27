a576f8206fa76ba99062133e0db231e7bef795e9
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-27 12:20:01 -0500

    helpers

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const path = require('path');
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const express = require("express");
 const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
@@ -17,13 +20,14 @@ const sess = {
   })
 };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
